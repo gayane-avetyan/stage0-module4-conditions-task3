ee1992d0b17882f9db8c490d458e79c783e3930b
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-04 13:59:47 +0400

    task 4_3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+            System.out.println("Vowel");
+        else if(character >= 65 && character <= 90 || character >= 97 && character <= 122)
+            System.out.println("Consonant");
+        else
+            System.out.println("wrong alphabet!");
     }
 }
